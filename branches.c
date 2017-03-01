15a0f74fd8c47a087e8681abf7dec2f50bbec3f3
saladass <panmihaurkin@gmail.com>
2017-03-01 09:28:13 +0700

    Updated using lib and desc of func

--- branches.c
+++ branches.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
-
-
+#include "prclib.h"
 
 int main() {
 
 	int num, perc;
+	int theOperRes;
 
 	printf("We can figure out percentage of number!\n");
 	printf("Enter number and percentage: ");
 	scanf("%d""%d", &num, &perc);
+
+	printf("The %d%% from %d is: %d\n", perc, num, theOperRes = per_calc(num, perc) );
 
  return 0;
 }
