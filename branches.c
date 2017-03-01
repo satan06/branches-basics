27ad837a60aa4f8469aa8b86a0d55c051d229b5f
saladass <panmihaurkin@gmail.com>
2017-03-01 08:58:46 +0700

    Format sourse code

---
+++ branches.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+
+int main() {
+
+	int num, perc;
+
+	printf("We can figure out percentage of number!\n");
+	printf("Enter number and percentage: ");
+	scanf("%d""%d", &num, &perc);
+
+ return 0;
+}
